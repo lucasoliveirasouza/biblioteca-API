b879cf0bfe82885d994c56a14e1cc290a5882161
lucasoliveirasouza <>
2022-06-11 18:20:19 -0300

    Criar requisições de LIVRO #10 #11 #12 #13

--- src/main/java/com/biblioteca/apirest/controllers/LivroController.java
+++ src/main/java/com/biblioteca/apirest/controllers/LivroController.java
@@ -1,4 +1,43 @@
 package com.biblioteca.apirest.controllers;
 
+
+import com.biblioteca.apirest.models.Categoria;
+import com.biblioteca.apirest.models.Livro;
+import com.biblioteca.apirest.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api")
 public class LivroController {
+
+    @Autowired
+    LivroRepository livroRepository;
+
+    @GetMapping("/livros")
+    public List<Livro> listaLivros(){
+        return livroRepository.findAll();
+    }
+
+    @GetMapping("/livro/{id}")
+    public Livro listaLivroUnico(@PathVariable(value="id") long id){
+        return livroRepository.findById(id);
+    }
+
+    @PostMapping("/livro")
+    public Livro salvaLivro(@RequestBody Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    @DeleteMapping("/livro")
+    public void deletaLivro(@RequestBody Livro livro) {
+        livroRepository.delete(livro);
+    }
+
+    @PutMapping("/livro")
+    public Livro atualizaLivro(@RequestBody Livro livro) {
+        return livroRepository.save(livro);
+    }
 }
