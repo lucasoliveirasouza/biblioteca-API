b13a002dd7945b173397c0a0a2ba0b00f180f7db
lucasoliveirasouza <>
2022-06-11 20:25:00 -0300

    Utilizar o Swagger #14

--- src/main/java/com/biblioteca/apirest/controllers/CategoriaController.java
+++ src/main/java/com/biblioteca/apirest/controllers/CategoriaController.java
@@ -2,40 +2,51 @@ package com.biblioteca.apirest.controllers;
 
 import com.biblioteca.apirest.models.Categoria;
 import com.biblioteca.apirest.repository.CategoriaRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Biblioteca")
 public class CategoriaController {
 
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @ApiOperation(value="Retorna uma lista com todas categorias")
     @GetMapping("/categorias")
     public List<Categoria> listaCategorias(){
+
         return categoriaRepository.findAll();
     }
 
+    @ApiOperation(value="Retorna uma única categoria com base no id")
     @GetMapping("/categoria/{id}")
     public Categoria listaCategoriaUnica(@PathVariable(value="id") long id){
+
         return categoriaRepository.findById(id);
     }
 
+    @ApiOperation(value="Adiciona uma nova categoria")
     @PostMapping("/categoria")
     public Categoria salvaCategoria(@RequestBody Categoria categoria) {
 
         return categoriaRepository.save(categoria);
     }
 
+    @ApiOperation(value="Deleta uma categoria")
     @DeleteMapping("/categoria")
     public void deletaCategoria(@RequestBody Categoria categoria) {
 
         categoriaRepository.delete(categoria);
     }
 
+    @ApiOperation(value="Atualiza uma categoria")
     @PutMapping("/categoria")
     public Categoria atualizaCategoria(@RequestBody Categoria categoria) {
 
--- src/main/java/com/biblioteca/apirest/controllers/LivroController.java
+++ src/main/java/com/biblioteca/apirest/controllers/LivroController.java
@@ -5,13 +5,17 @@ import com.biblioteca.apirest.models.Categoria;
 import com.biblioteca.apirest.models.Livro;
 import com.biblioteca.apirest.repository.CategoriaRepository;
 import com.biblioteca.apirest.repository.LivroRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Biblioteca")
 public class LivroController {
 
     @Autowired
@@ -20,11 +24,14 @@ public class LivroController {
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @ApiOperation(value="Retorna uma lista de todos livros")
     @GetMapping("livros")
     public List<Livro> listaLivros(){
+
         return livroRepository.findAll();
     }
 
+    @ApiOperation(value="Retorna uma lista de todos livros de uma categoria com base no ID dela")
     @GetMapping("categoria/{id}/livros")
     public List<Livro> listaLivrosCategoria(@PathVariable("id") long id){
         Categoria categoria = categoriaRepository.findById(id);
@@ -32,6 +39,7 @@ public class LivroController {
         return livros;
     }
 
+    @ApiOperation(value="Cadastra um novo livro em uma categoria com base no ID dela")
     @PostMapping("categoria/{id}/livro")
     public Livro salvaLivro(@RequestBody Livro livro, @PathVariable("id") long id) {
         Categoria categoria = categoriaRepository.findById(id);
@@ -39,6 +47,7 @@ public class LivroController {
         return livroRepository.save(livro);
     }
 
+    @ApiOperation(value="Deleta um livro de uma categoria com base no ID dela")
     @DeleteMapping("categoria/{id}/livro")
     public void deletaLivro(@RequestBody Livro livro, @PathVariable("id") long id) {
         Categoria categoria = categoriaRepository.findById(id);
@@ -46,6 +55,7 @@ public class LivroController {
         livroRepository.delete(livro);
     }
 
+    @ApiOperation(value="Atualiza um livro de uma categoria com base no ID dela")
     @PutMapping("categoria/{id}/livro")
     public Livro atualizaLivro(@RequestBody Livro livro, @PathVariable("id") long id) {
         Categoria categoria = categoriaRepository.findById(id);
