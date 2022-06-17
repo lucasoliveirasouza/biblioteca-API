5cfa073c3a789df382bc3c4e91f9ce174eb3b186
lucasoliveirasouza <>
2022-06-17 12:03:29 -0300

    Ajustar método POST em livro #23

--- src/main/java/com/biblioteca/apirest/controllers/LivroController.java
+++ src/main/java/com/biblioteca/apirest/controllers/LivroController.java
@@ -18,8 +18,8 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping(value="/api")
-@Api(value="API REST Biblioteca")
+@RequestMapping(value = "/api")
+@Api(value = "API REST Biblioteca")
 public class LivroController {
 
     @Autowired
@@ -34,56 +34,71 @@ public class LivroController {
     @Autowired
     EditoraRepository editoraRepository;
 
-    @ApiOperation(value="Retorna uma lista de todos livros")
+    @ApiOperation(value = "Retorna uma lista de todos livros")
     @GetMapping("livros")
-    public List<Livro> listaLivros(){
+    public List<Livro> listaLivros() {
 
         return livroRepository.findAll();
     }
 
-    @ApiOperation(value="Cadastra um novo livro em uma categoria com base no ID dela")
-    @PostMapping("categoria/{id}/livro")
-    public Livro salvaLivro(@RequestBody Livro livro, @PathVariable("id") long id) {
-        Categoria categoria = categoriaRepository.findById(id);
+    @ApiOperation(value = "Cadastra um novo livro em uma categoria com base no ID dela")
+    @PostMapping("livro/{id_categoria}/{id_autor}/{id_editora}")
+    public Livro salvaLivro(@RequestBody Livro livro, @PathVariable("id_categoria") long id_categoria, @PathVariable("id_autor") long id_autor, @PathVariable("id_editora") long id_editora) {
+        Categoria categoria = categoriaRepository.findById(id_categoria);
         livro.setCategoria(categoria);
+
+        Autor autor = autorRepository.findById(id_autor);
+        livro.setAutor(autor);
+
+        Editora editora = editoraRepository.findById(id_editora);
+        livro.setEditora(editora);
+
         return livroRepository.save(livro);
     }
 
-    @ApiOperation(value="Deleta um livro de uma categoria com base no ID dela")
-    @DeleteMapping("categoria/{id}/livro")
-    public void deletaLivro(@RequestBody Livro livro, @PathVariable("id") long id) {
+    @ApiOperation(value = "Deleta um livro com base no id dele")
+    @DeleteMapping("livro/{id}/")
+    public void deletaLivro(@PathVariable("id") long id) {
         Categoria categoria = categoriaRepository.findById(id);
         livro.setCategoria(categoria);
         livroRepository.delete(livro);
     }
 
-    @ApiOperation(value="Atualiza um livro de uma categoria com base no ID dela")
-    @PutMapping("categoria/{id}/livro")
-    public Livro atualizaLivro(@RequestBody Livro livro, @PathVariable("id") long id) {
-        Categoria categoria = categoriaRepository.findById(id);
+    @ApiOperation(value = "Atualiza um livro de uma categoria com base no ID dela")
+    @PutMapping("livro/{id_categoria}/{id_autor}/{id_editora}")
+    public Livro atualizaLivro(@RequestBody Livro livro, @PathVariable("id_categoria") long id_categoria, @PathVariable("id_autor") long id_autor, @PathVariable("id_editora") long id_editora) {
+        Categoria categoria = categoriaRepository.findById(id_categoria);
         livro.setCategoria(categoria);
+
+        Autor autor = autorRepository.findById(id_autor);
+        livro.setAutor(autor);
+
+        Editora editora = editoraRepository.findById(id_editora);
+        livro.setEditora(editora);
+
+
         return livroRepository.save(livro);
     }
 
-    @ApiOperation(value="Retorna uma lista de todos livros de uma categoria com base no ID dela")
+    @ApiOperation(value = "Retorna uma lista de todos livros de uma categoria com base no ID dela")
     @GetMapping("categoria/{id}/livros")
-    public List<Livro> listaLivrosCategoria(@PathVariable("id") long id){
+    public List<Livro> listaLivrosCategoria(@PathVariable("id") long id) {
         Categoria categoria = categoriaRepository.findById(id);
         List<Livro> livros = livroRepository.findByCategoria(categoria);
         return livros;
     }
 
-    @ApiOperation(value="Retorna uma lista de todos livros de um autor com base no ID dele")
+    @ApiOperation(value = "Retorna uma lista de todos livros de um autor com base no ID dele")
     @GetMapping("autor/{id}/livros")
-    public List<Livro> listaLivrosAutor(@PathVariable("id") long id){
+    public List<Livro> listaLivrosAutor(@PathVariable("id") long id) {
         Autor autor = autorRepository.findById(id);
         List<Livro> livros = livroRepository.findByAutor(autor);
         return livros;
     }
 
-    @ApiOperation(value="Retorna uma lista de todos livros de uma editora com base no ID dela")
+    @ApiOperation(value = "Retorna uma lista de todos livros de uma editora com base no ID dela")
     @GetMapping("editora/{id}/livros")
-    public List<Livro> listaLivrosEditora(@PathVariable("id") long id){
+    public List<Livro> listaLivrosEditora(@PathVariable("id") long id) {
         Editora editora = editoraRepository.findById(id);
         List<Livro> livros = livroRepository.findByEditora(editora);
         return livros;
