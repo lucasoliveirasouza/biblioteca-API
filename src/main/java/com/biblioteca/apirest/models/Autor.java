420c9c841c3143115ebb04270a1d6523d6b45b26
lucasoliveirasouza <>
2022-06-16 21:59:19 -0300

    Criar modelo de autor #16

---
+++ src/main/java/com/biblioteca/apirest/models/Autor.java
@@ -0,0 +1,33 @@
+package com.biblioteca.apirest.models;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="TB_AUTOR")
+public class Autor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
