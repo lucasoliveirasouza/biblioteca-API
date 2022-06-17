7414218586df674ba8e6ab2bb10798994b0b8bd3
lucasoliveirasouza <>
2022-06-17 17:56:18 -0300

    Configurar a controller de auth #29

--- src/main/java/com/biblioteca/apirest/controllers/CategoriaController.java
+++ src/main/java/com/biblioteca/apirest/controllers/CategoriaController.java
@@ -5,6 +5,7 @@ import com.biblioteca.apirest.repository.CategoriaRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
