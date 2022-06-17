f9b42a4806e4b412baa1f7924a95d179a0c97993
lucasoliveirasouza <>
2022-06-17 17:30:59 -0300

    Criar as classes do security #27

--- src/main/java/com/biblioteca/apirest/security/jwt/JwtUtils.java
+++ src/main/java/com/biblioteca/apirest/security/jwt/JwtUtils.java
@@ -1,16 +1,25 @@
 package com.biblioteca.apirest.security.jwt;
 
+import com.biblioteca.apirest.security.services.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
 
 @Component
 public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${spring.app.jwtSecret}")
+    @Value("${biblioteca.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${spring.app.jwtExpirationMs}")
+    @Value("${biblioteca.app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
